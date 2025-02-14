e3292336932910e7462360ebc4afc4502ca78108
Amir2244 <ameryosf224466@gmail.com>
2025-02-14 22:34:58 +0300

    Add Borrowing Entity

---
+++ src/main/java/library/entities/BorrowingRecord.java
@@ -0,0 +1,35 @@
+package library.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+public class BorrowingRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patron_id", nullable = false)
+    private Patron patron;
+
+    @Column(name = "borrow_date", nullable = false)
+    private LocalDate borrowDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+
+    public BorrowingRecord() {}
+}
